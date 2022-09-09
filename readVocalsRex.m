e465a1fb5dd1f8c58544498b1375f29ee39225d7
anbu8374 <anbu8374@macd3715.lasp.colorado.edu>
2022-09-09 16:59:21 -0600

    updates to the scratch analysis

--- readVocalsRex.m
+++ readVocalsRex.m
@@ -14,32 +14,81 @@ time = ncread(filename, 'Time');                                                
 % [hours, minutes]
 startTime = [str2double(info.Variables(1).Attributes(3).Value(26:27)),...
     str2double(info.Variables(1).Attributes(3).Value(29:30))];      % Start time of the data log in UTC
-drop_size_dist = ncread(filename, 'CCDP_RWO');                                                                       % Measured in #/micron/cm^3    
-lat = ncread(filename, 'LAT');                                                                                          % Meausred in degrees North
-long = ncread(filename, 'LON');                                                                                         % Meausred in degrees East
-altitude = ncread(filename, 'ALTX');                                                                                    % Measured in meters above sea level
 
+% ----------------------------------------------------------------
+% ----------- cloud droplet probe measurements (CDP) -------------
+% ----------------------------------------------------------------
 
-
-
-
+drop_size_dist_CDP = ncread(filename, 'CCDP_RWO');                                                                       % Measured in #/micron/cm^3    
 
 % The droplet number concentration is filtered into 30 bins from 2 to 52
 % microns
 % Below 20 microns, the bins are spaced by 1.18 microns
 % above 22 microns, the bins are spaced by 2.28 microns
-drop_bins = info.Variables(120).Attributes(10).Value;                        % microns
+drop_bins_CDP = info.Variables(120).Attributes(10).Value;                        % microns
 
 % The data tells us which bins to take
-first_bin = info.Variables(120).Attributes(8).Value;
-last_bin = info.Variables(120).Attributes(9).Value;
+first_bin_CDP = info.Variables(120).Attributes(8).Value;
+last_bin_CDP = info.Variables(120).Attributes(9).Value;
 
-drop_bins = drop_bins(first_bin:last_bin);                                  % microns
-drop_size_dist = drop_size_dist(first_bin:last_bin,1,:);
+drop_bins_CDP = drop_bins_CDP(first_bin_CDP:last_bin_CDP);                                  % microns
+drop_size_dist_CDP = drop_size_dist_CDP(first_bin_CDP:last_bin_CDP,1,:);
+
+
+
+% ----------------------------------------------------------------
+% ---------- Two-Dimensional Optical array probe (2DC) -----------
+% ----------------------------------------------------------------
+
+% This droplet probe filters droplets into 64 different size bins. The bins
+% range from 25 microns to 1560 microns. 
+
+% ** IMPORTANT ** The first bin of the 2DC probe overlaps with the last bin
+% of the CDP. So we ignore it. (See Painemal and Zuidema paragraph 15.)
+
+drop_size_dist_2DC = ncread(filename, 'C1DCA_RPC');                                             % Measured in #/micron/L  - where I think L stands for liters. 1 liter = 1000 cubic cm
+
+% convert the 2DC data to inverse cubic centimeters
+drop_size_dist_2DC = drop_size_dist_2DC ./ 1000;                                                % # of droplets/microns/cm^3
+
+% Grab the droplet size bins
+% These are the boundaries that define each bin size
+drop_bins_2DC = info.Variables(119).Attributes(12).Value;                        % microns
+
+% The data tells us which bins to take
+first_bin_2DC = double(info.Variables(119).Attributes(10).Value);
+last_bin_2DC = double(info.Variables(119).Attributes(11).Value);
+
+drop_bins_2DC = drop_bins_2DC(first_bin_2DC:last_bin_2DC);                                  % microns
+drop_size_dist_2DC = drop_size_dist_2DC(first_bin_2DC:last_bin_2DC,1,:);
+
+
+% Grab position and timing data
+lat = ncread(filename, 'LAT');                                                                                          % Meausred in degrees North
+long = ncread(filename, 'LON');                                                                                         % Meausred in degrees East
+altitude = ncread(filename, 'ALTX');                                                                                    % Measured in meters above sea level
+
+
+% lets grab the shortwave and longwave radiance looking down and looking up
+shortwave_top = ncread(filename, 'SWT');
+shortwave_bot = ncread(filename, 'SWB');
+
+longwave_top = ncread(filename,'IRTC');              % These are corrected longwave irradiance values
+longwave_bot = ncread(filename, 'IRBC');
+
+
+
+%     ***** IMPORTANT STEP *****
+
+% Combine data from both droplet probes to get the total droplet size
+% distribution
+
 
 % Lets compute the effective radius, which is the 3rd moment to the 2nd
 % moment
-nr = reshape(drop_size_dist,length(drop_bins),[]);
+drop_bins = [drop_bins_CDP, drop_bins_2DC];
+
+nr = [reshape(drop_size_dist_CDP,length(drop_bins_CDP),[]); reshape(drop_size_dist_2DC,length(drop_bins_2DC),[])];
 
 droplet_matrix = repmat((drop_bins)', 1, length(time));            % microns
 
@@ -70,15 +119,21 @@ lwc = 4/3 * pi *  rho_lw * sum(nr .* (droplet_matrix./1e4).^3,1);               
 % the data does not extend through all 31 rows. 
 
 vocalsRex.nr = nr;
+vocalsRex.drop_bins = drop_bins;
 vocalsRex.Nc = total_Nc;
 vocalsRex.lwc = lwc;
 vocalsRex.time = time;
 vocalsRex.startTime = startTime;                                  % We have to assume that this is in UTC time as well
-vocalsRex.drop_bins = drop_bins;
 vocalsRex.latitude = lat;
 vocalsRex.longitude = long;
 vocalsRex.altitude = altitude;
 vocalsRex.re = re;
+vocalsRex.SWT = shortwave_top;
+vocalsRex.SWB = shortwave_bot;
+vocalsRex.LWT = longwave_top;
+vocalsRex.LWB = longwave_bot;
+
+
 
 
 
--- scratch_VR_calcs.m
+++ scratch_VR_calcs.m
@@ -1,7 +1,8 @@
 %% Scratch work to figure out why my calcualtions of VOCALS Rex data differ from Painemal and Zudema
 
 % By Andrew John Buggee
-%%
+
+%%  Grab VOCALS-REx Data
 
 clear variables
 
@@ -11,6 +12,17 @@ scriptPlotting_wht;
 filename = 'RF11.20081109.125700_213600.PNI.nc';
 % load vocals rex data
 vr = readVocalsRex(filename);
+
+%% Grab MODIS data
+
+% This is the modis data set taken on day 314 at decimal time 0.611
+modis_folderName = './MODIS_data/2008_11_09/';
+
+
+[modis,L1B_500m_fileName] = retrieveMODIS_data(modis_folderName);
+
+modis_dataTime = (modis.time(1)*3600 + modis.time(2)*60)/86400;
+
 
 %% Painemal and Zudema use decimal time
 % This is used to identify when along a flights path they assumed a cloud
@@ -29,27 +41,69 @@ startTime_dec = (vr.startTime(1)*3600 + vr.startTime(2)*60)/86400;              
 
 pz_secSinceStart = floor((pz_time - startTime_dec)*sec_per_day);                               % seconds since flight started up to the time indicated by painemal and zudema
 
+modis_secSinceStart = floor((modis_dataTime - startTime_dec)*sec_per_day);
+
+
 % Define the number of discrete points in time you wish to look at. The
 % center point will be at the start time calculated above
 windowLength = 100;
 
-%% Make a plot of the number concentration and effective radius around time = 6256 
+% Using the time defined above, find the location in space closest to
+% the airplanes location
+
+% Using lat and long with can minimize the euclidean norm
+[minDist, index_minDist] = min(sqrt((modis.geo.lat - vr.latitude(pz_secSinceStart)).^2 + (modis.geo.long - vr.longitude(pz_secSinceStart)).^2), [], 'all');
+
+% grab the value for droplet size at the location closest to the planes
+% locaiton at the time specified above
+re_16_modis = modis.cloud.effRadius16(index_minDist);
+re_17_modis = modis.cloud.effRadius17(index_minDist);
+
+%% Make a plot of the number concentration and effective radius aat the time defined above
 
 % I want a subplot with the number concentration and altitude, and the
 % effective radius with altitude
+nice_blue = [0 0.4470 0.741];
 
 figure; subplot(1,2,1)
 semilogy(vr.time, vr.re); xlabel('Time (sec)'); ylabel('r_{e} (\mum)')
-grid on; grid minor; hold on; yyaxis right; plot(vr.time, vr.altitude); ylabel('Altitude (m agl)')
+grid on; grid minor; hold on; 
+
+% Plot the modis droplet estimate as a constant vertical line
+xl0 = yline(re_16_modis,'--',['MODIS $$r_{e} = $$',num2str(re_16_modis), '$$\mu m$$'], 'Fontsize',18, 'Interpreter','latex','LineWidth',2,'Color',nice_blue);
+xl0.LabelVerticalAlignment = 'top';
+
+% now plot the altitude of the plane on the right hand side
+yyaxis right; plot(vr.time, vr.altitude); ylabel('Altitude (m agl)')
 % zoom in on a particular window
 
 xlim([pz_secSinceStart - windowLength/2, pz_secSinceStart + windowLength/2]);
+
+
+
+% plot a vertical line marking the bottom of the cloud
+% This occurs when the LWC is atleast 0.03 c/m^3
+lwc_lim = 0.03;                                                 % grams/m^3 - lower limit defining cloud base
+
+LWC_window_index = pz_secSinceStart - windowLength/4 : pz_secSinceStart + windowLength/4;
+LWC_window = vr.lwc(LWC_window_index);
+[minVal, Ind_minVal] = min(abs(LWC_window - lwc_lim));
+
+% Plot a vertical line!
+xl = xline(vr.time(LWC_window_index(1) + Ind_minVal -1),'--','$$LWC = 0.03\; g/m^{3}$$', 'Fontsize',18, 'Interpreter','latex','LineWidth',2);
+xl.LabelVerticalAlignment = 'top';
+
 
 subplot(1,2,2)
 semilogy(vr.time, vr.Nc); xlabel('Time (sec)'); ylabel('N_{c} (cm^{-3})')
 grid on; grid minor; hold on; yyaxis right; plot(vr.time, vr.altitude); ylabel('Altitude (m agl)')
 % zoom in on a particular window
 xlim([pz_secSinceStart - windowLength/2, pz_secSinceStart + windowLength/2]);
+
+% Plot a vertical line!
+xl = xline(vr.time(LWC_window_index(1) + Ind_minVal -1),'--','$$LWC = 0.03\; g/m^{3}$$', 'Fontsize',18, 'Interpreter','latex','LineWidth',2);
+xl.LabelVerticalAlignment = 'top';
+xl.LabelHorizontalAlignment = "left";
 
 set(gcf, 'Position', [0 0 1300 400])
 
@@ -107,7 +161,31 @@ end
 
 
 
+%% Lets plot the time series of all clouds found in the above section
 
+figure;
+
+colororder('default')
+
+for ii = 1:length(cloud_index)
+    hold on
+    plot(vr.time(cloud_index{ii}), vr.re(cloud_index{ii}),'Color',[0, 0.4470, 0.7410]); 
+   
+end
+xlabel('Time (sec)'); ylabel('r_{e} (\mum)')
+
+yyaxis right
+
+for ii = 1:length(cloud_index)
+    hold on; 
+    plot(vr.time(cloud_index{ii}), vr.altitude(cloud_index{ii}), 'Color',[0.8500, 0.3250, 0.0980],'LineStyle','-'); 
+end
+ylabel('Altitude (m agl)')
+set(gca,'ycolor',[0.8500, 0.3250, 0.0980]) 
+
+grid on; grid minor; 
+
+set(gcf, 'Position', [0 0 1300 400])
 
 
 
@@ -118,3 +196,4 @@ end
 figure;
 histogram(vr.nr(:,pz_secSinceStart), vr.drop_bins); xlabel('Droplet Size (\mum)'); ylabel('n(r) (cm^{-3})')
 grid on; grid minor; hold on;
+set(gca,'YScale','log')
